6eb2e42627b3cfb4272729287daa950c55dd00a4
Lorena Garcia <lorenaamesqueta@gmail.com>
2024-10-18 21:08:04 -0500

    admin

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -13,28 +13,13 @@ function abrirModalHistorial(button) {
 
 console.log("register.js cargado correctamente");
 
-function funcionDeEliminar(gatitoId) {
-    if (confirm("¿Estás seguro de que deseas eliminar este gatito?")) {
-        fetch(`/admin/eliminar/${gatitoId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': document.querySelector('meta[name="_csrf"]').getAttribute('content') // Asegúrate de tener este meta tag en tu HTML
-            }
-        })
-        .then(response => {
-            if (response.ok) {
-                alert("Gatito eliminado exitosamente");
-                window.location.reload();  // Recargar la página para reflejar los cambios
-            } else {
-                alert("Error al eliminar el gatito");
-            }
-        })
-        .catch(error => {
-            console.error("Error al eliminar el gatito:", error);
-        });
-    }
+function confirmEliminar() {
+    return confirm("¿Estás seguro de que deseas eliminar este gatito?");
 }
+
+
+
+
 
 
 
